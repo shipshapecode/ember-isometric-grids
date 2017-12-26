40e1b98db5369437bc2261c5ca55d4fb136a749b
Robert Wagner <rwwagner90@gmail.com>
2017-12-26 09:20:12 -0500

    Initial commit

---
+++ app/components/iso-layer/component.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-isometric-grids/components/iso-layer/component';
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -14,6 +14,16 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
+  app.import('vendor/modernizr.custom.js');
+  app.import('vendor/imagesloaded.pkgd.min.js');
+  app.import('vendor/masonry.pkgd.min.js');
+  app.import('vendor/dynamics.min.js', {
+    type: 'vendor',
+    prepend: true
+  });
+  app.import('vendor/classie.js');
+  app.import('vendor/animOnScroll.js');
+  app.import('vendor/main.js');
 
   return app.toTree();
 };
