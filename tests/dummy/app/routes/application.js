bcd28c2b8580a4c53e098f34374baa4f7412e3ba
Robert Wagner <rwwagner90@gmail.com>
2017-12-31 10:58:13 -0500

    Finish moving grid items to JS

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -86,6 +86,486 @@ export default Route.extend({
         img: 'img/Dribbble1/1.jpg',
         link: 'https://dribbble.com/forefathers',
         title: 'Forefathers'
+      },
+      {
+        alt: '021',
+        img: 'img/Dribbble1/2_1.jpg',
+        link: 'https://dribbble.com/JulienLavallee',
+        title: 'Julien Lavallee'
+      },
+      {
+        alt: '03',
+        img: 'img/Dribbble1/3.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '042',
+        img: 'img/Dribbble1/4_2.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
+      },
+      {
+        alt: '011',
+        img: 'img/Dribbble1/1_1.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '02',
+        img: 'img/Dribbble1/2.jpg',
+        link: 'https://dribbble.com/JulienLavallee',
+        title: 'Julien Lavallee'
+      },
+      {
+        alt: '031',
+        img: 'img/Dribbble1/3_1.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '04',
+        img: 'img/Dribbble1/4.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
+      },
+      {
+        alt: '012',
+        img: 'img/Dribbble1/1_2.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '022',
+        img: 'img/Dribbble1/2_2.jpg',
+        link: 'https://dribbble.com/JulienLavallee',
+        title: 'Julien Lavallee'
+      },
+      {
+        alt: '032',
+        img: 'img/Dribbble1/3_2.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '013',
+        img: 'img/Dribbble1/1_3.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '041',
+        img: 'img/Dribbble1/4_1.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
+      },
+      {
+        alt: '01',
+        img: 'img/Dribbble1/1.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '021',
+        img: 'img/Dribbble1/2_1.jpg',
+        link: 'https://dribbble.com/JulienLavallee',
+        title: 'Julien Lavallee'
+      },
+      {
+        alt: '03',
+        img: 'img/Dribbble1/3.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '042',
+        img: 'img/Dribbble1/4_2.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
+      },
+      {
+        alt: '011',
+        img: 'img/Dribbble1/1_1.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '02',
+        img: 'img/Dribbble1/2.jpg',
+        link: 'https://dribbble.com/JulienLavallee',
+        title: 'Julien Lavallee'
+      },
+      {
+        alt: '031',
+        img: 'img/Dribbble1/3_1.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '04',
+        img: 'img/Dribbble1/4.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
+      },
+      {
+        alt: '012',
+        img: 'img/Dribbble1/1_2.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '022',
+        img: 'img/Dribbble1/2_2.jpg',
+        link: 'https://dribbble.com/JulienLavallee',
+        title: 'Julien Lavallee'
+      },
+      {
+        alt: '032',
+        img: 'img/Dribbble1/3_2.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '013',
+        img: 'img/Dribbble1/1_3.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '041',
+        img: 'img/Dribbble1/4_1.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
+      },
+      {
+        alt: '01',
+        img: 'img/Dribbble1/1.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '021',
+        img: 'img/Dribbble1/2_1.jpg',
+        link: 'https://dribbble.com/JulienLavallee',
+        title: 'Julien Lavallee'
+      },
+      {
+        alt: '03',
+        img: 'img/Dribbble1/3.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '042',
+        img: 'img/Dribbble1/4_2.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
+      },
+      {
+        alt: '011',
+        img: 'img/Dribbble1/1_1.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '02',
+        img: 'img/Dribbble1/2.jpg',
+        link: 'https://dribbble.com/JulienLavallee',
+        title: 'Julien Lavallee'
+      },
+      {
+        alt: '031',
+        img: 'img/Dribbble1/3_1.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '04',
+        img: 'img/Dribbble1/4.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
+      },
+      {
+        alt: '012',
+        img: 'img/Dribbble1/1_2.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '022',
+        img: 'img/Dribbble1/2_2.jpg',
+        link: 'https://dribbble.com/JulienLavallee',
+        title: 'Julien Lavallee'
+      },
+      {
+        alt: '032',
+        img: 'img/Dribbble1/3_2.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '013',
+        img: 'img/Dribbble1/1_3.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '041',
+        img: 'img/Dribbble1/4_1.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
+      },
+      {
+        alt: '01',
+        img: 'img/Dribbble1/1.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '021',
+        img: 'img/Dribbble1/2_1.jpg',
+        link: 'https://dribbble.com/JulienLavallee',
+        title: 'Julien Lavallee'
+      },
+      {
+        alt: '03',
+        img: 'img/Dribbble1/3.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '042',
+        img: 'img/Dribbble1/4_2.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
+      },
+      {
+        alt: '011',
+        img: 'img/Dribbble1/1_1.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '02',
+        img: 'img/Dribbble1/2.jpg',
+        link: 'https://dribbble.com/JulienLavallee',
+        title: 'Julien Lavallee'
+      },
+      {
+        alt: '031',
+        img: 'img/Dribbble1/3_1.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '04',
+        img: 'img/Dribbble1/4.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
+      },
+      {
+        alt: '012',
+        img: 'img/Dribbble1/1_2.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '022',
+        img: 'img/Dribbble1/2_2.jpg',
+        link: 'https://dribbble.com/JulienLavallee',
+        title: 'Julien Lavallee'
+      },
+      {
+        alt: '032',
+        img: 'img/Dribbble1/3_2.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '013',
+        img: 'img/Dribbble1/1_3.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '041',
+        img: 'img/Dribbble1/4_1.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
+      },
+      {
+        alt: '01',
+        img: 'img/Dribbble1/1.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '021',
+        img: 'img/Dribbble1/2_1.jpg',
+        link: 'https://dribbble.com/JulienLavallee',
+        title: 'Julien Lavallee'
+      },
+      {
+        alt: '03',
+        img: 'img/Dribbble1/3.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '042',
+        img: 'img/Dribbble1/4_2.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
+      },
+      {
+        alt: '011',
+        img: 'img/Dribbble1/1_1.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '02',
+        img: 'img/Dribbble1/2.jpg',
+        link: 'https://dribbble.com/JulienLavallee',
+        title: 'Julien Lavallee'
+      },
+      {
+        alt: '031',
+        img: 'img/Dribbble1/3_1.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '04',
+        img: 'img/Dribbble1/4.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
+      },
+      {
+        alt: '012',
+        img: 'img/Dribbble1/1_2.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '022',
+        img: 'img/Dribbble1/2_2.jpg',
+        link: 'https://dribbble.com/JulienLavallee',
+        title: 'Julien Lavallee'
+      },
+      {
+        alt: '032',
+        img: 'img/Dribbble1/3_2.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '013',
+        img: 'img/Dribbble1/1_3.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '041',
+        img: 'img/Dribbble1/4_1.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
+      },
+      {
+        alt: '032',
+        img: 'img/Dribbble1/3_2.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '013',
+        img: 'img/Dribbble1/1_3.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '041',
+        img: 'img/Dribbble1/4_1.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
+      },
+      {
+        alt: '01',
+        img: 'img/Dribbble1/1.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '021',
+        img: 'img/Dribbble1/2_1.jpg',
+        link: 'https://dribbble.com/JulienLavallee',
+        title: 'Julien Lavallee'
+      },
+      {
+        alt: '03',
+        img: 'img/Dribbble1/3.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '042',
+        img: 'img/Dribbble1/4_2.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
+      },
+      {
+        alt: '011',
+        img: 'img/Dribbble1/1_1.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '02',
+        img: 'img/Dribbble1/2.jpg',
+        link: 'https://dribbble.com/JulienLavallee',
+        title: 'Julien Lavallee'
+      },
+      {
+        alt: '031',
+        img: 'img/Dribbble1/3_1.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '04',
+        img: 'img/Dribbble1/4.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
+      },
+      {
+        alt: '012',
+        img: 'img/Dribbble1/1_2.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '022',
+        img: 'img/Dribbble1/2_2.jpg',
+        link: 'https://dribbble.com/JulienLavallee',
+        title: 'Julien Lavallee'
+      },
+      {
+        alt: '032',
+        img: 'img/Dribbble1/3_2.jpg',
+        link: 'https://dribbble.com/creativemints',
+        title: 'Mike | Creative Mints'
+      },
+      {
+        alt: '013',
+        img: 'img/Dribbble1/1_3.jpg',
+        link: 'https://dribbble.com/forefathers',
+        title: 'Forefathers'
+      },
+      {
+        alt: '041',
+        img: 'img/Dribbble1/4_1.jpg',
+        link: 'https://dribbble.com/Radium',
+        title: 'Cosmin Capitanu'
       }
     ];
   }
